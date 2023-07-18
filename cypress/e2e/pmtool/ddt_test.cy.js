342658fddc8c6b228e6a33b0f68ea64c90d7e3ba
Ivana Maříková <ivanamarikova@Ivana-MacBook-Air.local>
2023-07-19 01:26:09 +0200

    hw

--- cypress/e2e/pmtool/ddt_test.cy.js
+++ cypress/e2e/pmtool/ddt_test.cy.js
@@ -1,5 +1,5 @@
 import newProjectData from '../../fixtures/new_project_data.json';
-import faker from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import moment from 'moment';
 import { LoginPage } from '../../page-objects/pmtool/login_page';
 import { NewProjectFormModal } from '../../page-objects/pmtool/new_project_form_modal';
@@ -10,21 +10,23 @@ describe("Data Driven Tests", () => {
             .openPmtool()
             .typeUsername(Cypress.env("pmtool_username"))
             .typePassword(Cypress.env("pmtool_password"))
-            .clickLogin()
+            .clickLoginButton()
             .clickProjects()
-            .clickNewProjectButton();
+            .clickAddNewProject();
     });
 
     newProjectData.forEach((project) => {
         it(`DDT project: ${project.description}`, () => {
-            //setup test data
-            const project_name =
-                project.name_prefix + faker.number.int({ max: 99999 });
+            const project_name = `${project.name_prefix}${faker.number.int({
+                max: 99999,
+            })}`;
+            cy.log(project_name);
             const start_date = getStartDate(project.start_date);
-            //test steps
+            cy.log(`Project start date: ${project.start_date}`);
+
             new NewProjectFormModal()
-                .selectPriority(project.priority)
                 .selectStatus(project.status)
+                .selectPriority(project.priority)
                 .typeProjectName(project_name)
                 .fillStartDate(start_date)
                 .typeDescription(project.description)
@@ -37,10 +39,10 @@ function getStartDate(start_date) {
     switch (start_date) {
         case "today":
             return moment().format("YYYY-MM-DD");
-        case "tomorrow":
-            return moment().add(1, "days").format("YYYY-MM-DD");
         case "yesterday":
             return moment().subtract(1, "days").format("YYYY-MM-DD");
+        case "tomorrow":
+            return moment().add(1, "days").format("YYYY-MM-DD");
         default:
             throw new Error("Invalid start date");
     }
