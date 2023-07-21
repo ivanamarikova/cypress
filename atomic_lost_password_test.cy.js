b6a3aa7e05502bbaaafec880da3d3381d4c5f66e
ivanamarikova <126821376+ivanamarikova@users.noreply.github.com>
2023-07-21 12:00:30 +0200

    Update atomic_lost_password_test.cy.js
    Přidání funkce context

--- atomic_lost_password_test.cy.js
+++ atomic_lost_password_test.cy.js
@@ -3,47 +3,54 @@ import { LostPasswordPage } from "../../page-objects/pmtool/lost_password_page";
 
 describe("Vytvoření atomických testů - PMTool - Lost Password page", () => {
     beforeEach(() => {
-        new LoginPage()
-            .openPmtool()
+        new LoginPage().openPmtool();
+        new LostPasswordPage().visit();
     });
 
-    it('Otevře PMTool - Password forgotten obrazovku', () => {
-        const lostPasswordPage = new LostPasswordPage();
-
-        lostPasswordPage
-            .clickBackForget()
-
-        // Kontroly prvků na stránce PMTool - Password forgotten obrazovk
-
-        cy.get("body").within(() => {
-
-            // Kontrola viditelnosti, atributu a placeholderu pro pole Username
-
-            cy.xpath(lostPasswordPage.usernameInput).should("be.visible");
-            cy.xpath(lostPasswordPage.usernameInput).should("have.attr", "placeholder", "Username");
-
-            // Kontrola viditelnosti, atributu a placeholderu pro pole Email
-
-            cy.xpath(lostPasswordPage.emailInput).should("be.visible");
-            cy.xpath(lostPasswordPage.emailInput).should("have.attr", "placeholder", "Email");
-
-            // Kontrola viditelnosti a textu pro tlačítko Send
-
-            cy.get(lostPasswordPage.sendButton).should("be.visible");
-            cy.get(lostPasswordPage.sendButton).should("contain.text", "Send");
+    context("UsernameInput tests", () => {
+        it("UsernameInput is visible", () => {
+            new LostPasswordPage().usernameInputIsVisible();
         });
 
-        // Kontrola viditelnosti tlačítka Back, loga a nadpisu
+        it("UsernameInput has placeholder", () => {
+            new LostPasswordPage().usernameInputHasPlaceholder();
+        });
+    });
 
-        cy.get(lostPasswordPage.backButton).should("be.visible");
-        cy.get(lostPasswordPage.logo).should("be.visible");
-        cy.get(lostPasswordPage.title).should("be.visible");
-        cy.get(lostPasswordPage.title).should("contain.text", "Restore Password");
+    context("EmailInput tests", () => {
+        it("EmailInput is visible", () => {
+            new LostPasswordPage().emailInputIsVisible();
+        });
 
-        //Kontrola URL
+        it("EmailInput has placeholder", () => {
+            new LostPasswordPage().emailInputHasPlaceholder();
+        });
+    });
 
+    context("Send Button tests", () => {
+        it("Send button is visible", () => {
+            new LostPasswordPage().sendButtonIsVisible();
+        });
 
-        cy.url().should("match", /http:\/\/tredgate.com\/pmtool\/index.php\?module=users\/restore_password/);
+        it("Send button has text", () => {
+            new LostPasswordPage().sendButtonHasText("Send");
+        });
+    });
+
+    context("Logo test", () => {
+        it("Logo is visible", () => {
+            new LostPasswordPage().logoIsVisible();
+        });
+    });
+
+    context("Title tests", () => {
+        it("Title is visible", () => {
+            new LostPasswordPage().titleIsVisible();
+        });
+
+        it("Title has text", () => {
+            new LostPasswordPage().titleHasText("Restore Password");
+        });
     });
 });
 
