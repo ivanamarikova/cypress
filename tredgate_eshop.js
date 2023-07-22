971981f186046f602d1613e9a79db14993ffe7a5
ivanamarikova <126821376+ivanamarikova@users.noreply.github.com>
2023-07-22 11:14:19 +0200

    Delete tredgate_eshop.js

--- tredgate_eshop.js
+++
@@ -1,7 +0,0 @@
-import { eshopBasePage } from "./eshop_base_page";
-
-export class TredgateEshop extends eshopBasePage {
-    constructor(path) {
-        super("route=product/product&product_id=40");
-    }
-}
