4bda8b35ea3fbc6dbbebf1f11b28761188f4382f
ivanamarikova <126821376+ivanamarikova@users.noreply.github.com>
2023-07-24 09:16:40 +0200

    Oprava

---
+++ cypress/page-objects/pmtool/projects_page.js
@@ -0,0 +1,16 @@
+export class ProjectsPage {
+    constructor() {
+        this.projectPageUrl = "http://tredgate.com/pmtool/index.php?module=items/items&path=21";
+        this.addProjectButton = "button[test_id="Add Project"]";
+    }
+    openprojectPageUrl() {
+        cy.visit(this.projectPageUrl);
+        return this;
+    }
+
+    clickaddProjectButton() {
+        cy.get(this.addProjectButton).click();
+        return this;
+    }
+
+}
