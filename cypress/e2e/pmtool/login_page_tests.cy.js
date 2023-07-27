8e768bc54c2921154808963a2b447ad70328342d
ivana.marikova <marikova.ivana@gmail.com>
2023-07-27 20:22:06 +0200

    github actions learning

--- cypress/e2e/pmtool/login_page_tests.cy.js
+++ cypress/e2e/pmtool/login_page_tests.cy.js
@@ -1,10 +1,21 @@
-import { LoginPage } from "/Users/ivanamarikova/GIT/cypress/cypress/page-objects/pmtool/login_page.js"
-describe("Login page tests", () => {
+describe("Login page tests", { tags: '@smoke' }, () => {
     beforeEach(() => {
         new LoginPage().openPmtool();
     });
 
     it("Page header has text 'Login'", () => {
         new LoginPage().pageHeaderHasText("Login");
+    })
+
+    it('Username has "Username" as placeholder', () => {
+        new LoginPage().usernameHasPlaceholder("Username");
+    });
+
+    it('Password has "Password" as placeholder', () => {
+        new LoginPage().passwordHasPlaceholder("Password");
+    });
+
+    it("Logo is visible", () => {
+        new LoginPage().logoIsVisible();
     });
 });
